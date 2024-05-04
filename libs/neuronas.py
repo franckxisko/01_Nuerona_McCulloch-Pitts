38c59aa4cfa8457bbe8257fda86f157771156ec9
franckxisko <franck26@gmail.com>
2024-05-04 15:12:21 -0600

    2do Commit

--- libs/neuronas.py
+++ libs/neuronas.py
@@ -1,5 +1,14 @@
 class Neurona_McCullochPittss_Todo_en_1:
-    def __init__(self):
+    def __init__(self, input1_valor, input2_valor, input3_valor):
         self.threshold = 1
+        self.input1 = input1_valor
+        self.input2 = input2_valor
+        self.input3 = input3_valor
+        
+def activacion (self):
+    if self.input1 + self.input2 + self.input3 >= self.threshold:
+        return 1
+    else:
+        return 0
         
         
