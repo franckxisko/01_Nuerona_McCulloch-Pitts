25aae7818428f58fffe111b645df20abb452c04a
franckxisko <franck26@gmail.com>
2024-05-10 22:49:36 -0600

    3 Commint - Dev

--- notebook/main.py
+++ notebook/main.py
@@ -3,8 +3,22 @@ sys.path.append('./libs')
 from neuronas import Neurona_McCullochPittss_Todo_en_1
 
 def main():
+    # D.1.1
+    # (a. -)
+    tiempo_libre = 1
+    motivacion = 0
+    suscripcion = 0
+    
+    neurona = Neurona_McCullochPittss_Todo_en_1(tiempo_libre, motivacion, suscripcion)
+    descision = neurona.activacion()
+    
     neuronas = Neurona_McCullochPittss_Todo_en_1()
     print(neuronas.threshold) 
+    
+    if descision == 1:
+        print("Si, ir a hacer ejercicios")
+    if descision == 0:
+        print("No hacer ejercicios")
     
     if __name__ == "__main__":
         main()
